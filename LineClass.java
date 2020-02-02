ac6a057d477153aa9c06a93a2c647c3a03565640
Thor C <tcuc@users.noreply.github.com>
2020-02-02 18:53:03 +0100

    not quite done.

--- LineClass.java
+++ LineClass.java
@@ -1,27 +1,94 @@
-import java.util.Vector;
-
-public class LineClass()
+public class LineClass
 {
-    private Vector data = new Vector<>();
-    private int score; 
+    private char[] gameData;
+    private int score = 0;
 
-    public void scoreingMath(Vector d)
+    private void calculateScore()
     {
         // some math to produce a sum. 
         // score should be set here. 
+        int roundNumber = 0;
+        for( int i=0; i < this.gameData.length; i++ )
+        {
+            if(roundNumber > 30)
+            {
+                break;
+            }
+            //char c = this.gameData[i];
+            switch (this.gameData[i])
+            {
+                case ' ':
+                    roundNumber++;
+                    continue;
+                case '/':
+                    score += getVal(i) + getVal(i +1);
+                case 'x':
+                    score += 10;
+                    int lookForward = 2;
+                    int nextIndex = 1;
+                    do
+                    {
+                        if(gameData[i + nextIndex] == ' ')
+                        {
+                            ++nextIndex;
+                            continue;
+                        }
+                        else
+                        {
+                            score += getVal(i + nextIndex);
+                            --lookForward;
+                            ++nextIndex;
+                        }
+                    }while(lookForward > 1 );
+                    break;
+                default:
+                score += getVal(i);
+            }
+            System.out.println(score);
+
+        }
+
     }
 
-    public int score()
+    public int getVal(int indexOfChar)
+    {
+        switch (gameData[indexOfChar])
+        {
+            //
+            case 'x':
+            return 10;
+            case '/':
+            // Find the number of pins knocked dow on the spare throw. 
+            return 10 - Character.getNumericValue(gameData[indexOfChar - 1]);
+            case '-':
+            return 0;
+            default:
+            return Character.getNumericValue(gameData[ indexOfChar ]);
+        }
+    }
+
+    public void loadGameData(String s)
+    {
+        this.gameData = s.toCharArray();
+        this.calculateScore();
+    }
+
+    public int getScore()
     {
         return score;
     }
 
    //TODO: ad a loadData method to avoid making multiple objects for each game.
 
-    public void LineClass(String scoreString)
+    public LineClass(String scoreString)
     {
-        // parse the string into the data vector for easy scoring math. 
-        // fill the data vector here. 
+        loadGameData(scoreString);
+    }
+
+    public static void main(String[] args)
+    {
+        LineClass test = new LineClass("1/ 18 x 11 1- 32 32 32 32 32-"); //"1/ 2- x 32 9- x 32 21"
+        System.out.println(test.getScore());
     }
 
 }
